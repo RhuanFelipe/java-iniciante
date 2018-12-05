27a7c66985dce564ef2638239ab184376c32a8b4
RhuanFelipe <rhuanfelsilva@gmail.com>
2018-12-05 10:18:59 -0300

    Primeiro código

---
+++ Primeiro_Projeto/src/unidade2/PrimeiroCodigo.java
@@ -0,0 +1,13 @@
+package unidade2;
+
+import javax.swing.JOptionPane;
+
+public class PrimeiroCodigo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!");
+		JOptionPane.showMessageDialog(null, "Olá Mundo!");
+	}
+
+}
